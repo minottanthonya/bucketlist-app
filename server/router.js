3c72c33ea6baa6adb7c6082c349438ff44ec477c
Tony Minott <minottanthonya@gmail.com>
2017-02-24 15:27:49 -0500

    finished module 8

--- server/router.js
+++ server/router.js
@@ -1,14 +1,19 @@
 var Auth = require('./controllers/auth');
-var User = require('./models/user');
+var passportService = require('./services/passport');
+var passport = require('passport');
+
+var requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app){
-    app.get('/', function(req, res, next){
+   
+    app.get('/', requireAuth, function(req, res){
         res.send("HELLO HOMEPAGE");
     });
     // Works and shows a message for /signup
     // app.get('/signup', function(req, res, next){
     //     res.send("Thank you for signing up!");
     // });
+    app.post('/signup', Auth.signup);
 }
 
 module.exports = function(app){
